afed05978f432af76f0a4d12b99a38c67a71b717
CrystalKnightCodes <CrystalKnightCodes@gmail.com>
2019-12-04 22:09:37 -0500

    Added Entry+Convenience file.  Deleted extraneous model

---
+++ Journal/Journal/Entry+Convenience.swift
@@ -0,0 +1,19 @@
+//
+//  Entry+Convenience.swift
+//  Journal
+//
+//  Created by Christy Hicks on 12/4/19.
+//  Copyright © 2019 Knight Night. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+extension Entry {
+    convenience init(title: String, detail: String? = nil, timeStamp: Date, identifier: String, context: NSManagedObjectContext = CoreDataStack.shared.mainContext) {
+        self.init(context: context)
+        self.title = title
+        self.detail = detail
+        self.timeStamp = timeStamp
+    }
+}
