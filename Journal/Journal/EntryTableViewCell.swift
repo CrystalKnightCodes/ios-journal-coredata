a7bd7a80e93a94d257e38091b554ed3c35b203fa
CrystalKnightCodes <CrystalKnightCodes@gmail.com>
2019-12-04 21:38:35 -0500

    Initial setup of Entry Cell

---
+++ Journal/Journal/EntryTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  EntryTableViewCell.swift
+//  Journal
+//
+//  Created by Christy Hicks on 12/4/19.
+//  Copyright © 2019 Knight Night. All rights reserved.
+//
+
+import UIKit
+
+class EntryTableViewCell: UITableViewCell {
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var detailLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
